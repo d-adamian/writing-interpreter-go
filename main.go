4d6b2cafc0119f1a95ff70f246d194bd929250d7
Dmitrii Adamian <d_adamian@novel-soft.com>
2025-05-29 15:59:30 +0300

    Evaluating Index Operator Expressions

--- main.go
+++ main.go
@@ -19,4 +19,4 @@ func main() {
 	repl.Start(os.Stdin, os.Stdout)
 }
 
-// Continue at page 174, section "Evaluating Index Operator Expressions"
+// Continue at page 177, section "Adding Built-in Functions for Arrays"
